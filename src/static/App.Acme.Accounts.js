5884179560ff9764253cbc043fa658dc566793d4
Adrian David Smith <sierra.as74@gmail.com>
2017-06-18 14:23:03 +0200

    Validation

--- src/static/App.Acme.Accounts.js
+++ src/static/App.Acme.Accounts.js
@@ -39,21 +39,72 @@
             });
 
             $(document).on("click", "#withdraw", function(){
-                self.accountNumber = $(this).data("account-number");
+
+                self.account = $(this).data("account");
+
+                if(!$("#withdraw-error").is(".hidden")){
+                    $("#withdraw-error").addClass("hidden");
+                }
+
                 $("#withdraw-modal").modal("toggle");
+
             });
 
             $(document).on("click", "#deposit", function(){
-                self.accountNumber = $(this).data("account-number");
+
+                self.account = $(this).data("account");
+
+                // reset the error message
+                if(!$("#deposit-error").is(".hidden")){
+                    $("#deposit-error").addClass("hidden");
+                }
+
                 $("#deposit-modal").modal("toggle");
+
             });
 
             $(document).on("click", "#deposit-confirm", function(){
-                console.log("here");
+
+                var valid = true;
+                var error = "";
+                var amount = parseInt($("#deposit-amount").val());
+
+                if(isNaN(amount)){
+                    error = "Please insert valid number";
+                    valid = false;
+                }
+
+                // validate here
+
+                if(valid === true){
+                    $("#deposit-modal").modal("toggle");
+                }
+                else{
+                    $("#deposit-error").removeClass("hidden");
+                    $("#deposit-error").html(error);
+                }
+
             });
 
             $(document).on("click", "#withdraw-confirm", function(){
-                console.log("here");
+
+                var valid = true;
+                var error = "";
+                var amount = parseInt($("#withdraw-amount").val());
+
+                if(isNaN(amount)){
+                    error = "Please insert valid number";
+                    valid = false;
+                }
+
+                if(valid === true){
+                    $("#withdraw-modal").modal("toggle");
+                }
+                else{
+                    $("#withdraw-error").removeClass("hidden");
+                    $("#withdraw-error").html(error);
+                }
+
             });
 
         },
@@ -88,8 +139,8 @@
 
                 // buttons with account number id
                 html += "<td>" +
-                        "<button type='button' id='withdraw' data-account-number='" + account["account_number"] + "' class='btn btn-primary'>Withdraw</button> " +
-                        "<button type='button' id='deposit' data-account-number='" + account["account_number"] + "' class='btn btn-info'>Deposit</button>" +
+                        "<button type='button' id='withdraw' data-account='" + JSON.stringify(account) + "' class='btn btn-primary'>Withdraw</button> " +
+                        "<button type='button' id='deposit' data-account='" + JSON.stringify(account) + "' class='btn btn-info'>Deposit</button>" +
                         "</td>";
 
                 html+= "</tr>";
